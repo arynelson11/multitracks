c39096f660ba5b4d350a588b39664c05aeef4972
Isabela <isabela@exemplo.com>
2026-02-21 12:18:15 -0300

    fix(auth): explicitly set google oauth redirect url to window.location.origin

--- src/components/AuthPage.tsx
+++ src/components/AuthPage.tsx
@@ -43,6 +43,9 @@ export function AuthPage({ }: AuthPageProps) {
         try {
             const { error } = await supabase!.auth.signInWithOAuth({
                 provider: 'google',
+                options: {
+                    redirectTo: window.location.origin
+                }
             });
             if (error) throw error;
         } catch (err: any) {
